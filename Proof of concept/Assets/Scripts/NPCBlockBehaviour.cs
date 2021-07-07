cff748a68722828cc493ae6e455c966c520b02b5
Luc <l.vidal@rubika-edu.com>
2021-07-07 10:57:15 +0200

    Expériences chelous pour les NPC blockeurs

---
+++ Proof of concept/Assets/Scripts/NPCBlockBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBlockBehaviour : MonoBehaviour
+{
+    public GameObject[] NPCs;
+    public GameObject[] BlockingPositions;
+    private Transform[] NormalPositions;
+
+    public void Start()
+    {
+
+
+        for (int a = 0; a <= NPCs.Length; a++)
+        { 
+
+        }
+    }
+    public void BlockTheWay()
+    {
+
+    }
+}
